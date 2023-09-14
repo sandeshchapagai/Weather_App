4627ed0871d29f495f4f473bd717c868a22bd2ca
Sandy <sandeshchapagai@users.noreply.github.com>
2023-09-14 16:56:27 +0545

    back added

--- lib/home.dart
+++ lib/home.dart
@@ -7,11 +7,24 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('vsd'),
+        title:Text('Weather'),
+        centerTitle:true,
+        titleTextStyle:TextStyle(color:Colors.white),
+        backgroundColor: Colors.transparent,
+        leading: IconButton(icon: Icon(Icons.search,size: 30,color: Colors.white,),
+        
+        onPressed: (){},),
+        actions: [
+          Container(
+            padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+            child: Image.asset('assests/background.jpg'),
+          )
+        ],
+        
       ),
       body: Container(
         height: 100,
-        color: Colors.blueGrey,
+        
       ),
     );
   }
