7476f8b41d1f5e3a9eec552fa53aa058d45e8b55
Sandy <sandeshchapagai@users.noreply.github.com>
2023-09-15 15:25:55 +0545

    make  a background a single image

--- lib/home.dart
+++ lib/home.dart
@@ -1,30 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  const Home({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:Text('Weather'),
-        centerTitle:true,
-        titleTextStyle:TextStyle(color:Colors.white),
-        backgroundColor: Colors.transparent,
-        leading: IconButton(icon: Icon(Icons.search,size: 30,color: Colors.white,),
-        
-        onPressed: (){},),
-        actions: [
-          Container(
-            padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
-            child: Image.asset('assests/background.jpg'),
-          )
-        ],
-        
+      extendBodyBehindAppBar: true,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: AppBar(
+          title: Text('Weather',
+          style:GoogleFonts.ubuntu(
+          textStyle:TextStyle(
+            fontWeight: FontWeight.w800,
+            fontSize: 30,
+          ),),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: IconButton(
+            icon: Icon(Icons.search, size: 35, color: Colors.white),
+            onPressed: () {},
+          ),
+          actions: [],
+
+        ),
       ),
-      body: Container(
-        height: 100,
-        
+      body: Stack(
+        children: [
+          // Background Image for Body
+          Opacity(
+            opacity: 0.9,
+            child: Image.asset(
+              'assets/images/background.jpg',
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            ),
+          ),
+          // Other content goes here
+          Container(
+            padding: EdgeInsets.fromLTRB(20, 100, 20, 20),
+            child: Center(
+              // Add your other widgets here
+              child: Text(
+                'Your Content Goes Here',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
